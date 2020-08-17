a4bb5ef0e2523a7c92223576edb12684416ccb3c
venkin84 <venkataraman.n.mail@gmail.com>
2020-08-18 00:04:10 +0530

    Initial Commit

---
+++ src/main/java/com/takeurq/repositoryservice/RepositoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.takeurq.repositoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepositoryServiceApplication.class, args);
+    }
+
+}
